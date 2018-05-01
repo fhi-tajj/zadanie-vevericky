98bc776c13212e3b84dc9490fc7435a39efca250
Igor Banduric <igor.banduric@atos.net>
2018-05-01 09:53:48 +0200

    Inicializacia kodu

---
+++ src/main/java/sk/euba/fhi/tajj/vevericky/Vevericky.java
@@ -0,0 +1,10 @@
+package sk.euba.fhi.tajj.vevericky;
+
+/**
+ * Created by Igor Banduric.
+ */
+public interface Vevericky {
+
+	public boolean veverickySaHraju(int teplota, boolean jeLeto);
+
+}
---
+++ src/test/java/sk/euba/fhi/tajj/vevericky/VeverickyTest.java
@@ -0,0 +1,23 @@
+package sk.euba.fhi.tajj.vevericky;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Igor Banduric.
+ */
+public class VeverickyTest {
+
+	private Vevericky vevericky; //FIXME
+
+	@Test
+	public void testDveTri() {
+		assertEquals( true, this.vevericky.veverickySaHraju( 20, false ) );
+		assertEquals( false, this.vevericky.veverickySaHraju( 30, false ) );
+		assertEquals( true, this.vevericky.veverickySaHraju( 35, true ) );
+		assertEquals( true, this.vevericky.veverickySaHraju( 30, true ) );
+		assertEquals( false, this.vevericky.veverickySaHraju( 10, false ) );
+		assertEquals( false, this.vevericky.veverickySaHraju( 10, true ) );
+	}
+}
